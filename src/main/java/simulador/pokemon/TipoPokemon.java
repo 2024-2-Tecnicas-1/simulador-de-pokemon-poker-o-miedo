b9d6b31d73ac4ff1b92b30aa382b8c86b4302c25
Juancho <judavidrami@gmail.com>
2024-11-20 20:07:43 -0500

    re-creacion de la clase tipo pokemon
    se volvio hacer ya que se estaba utilizando class en vez de Enum

--- src/main/java/simulador/pokemon/TipoPokemon.java
+++ src/main/java/simulador/pokemon/TipoPokemon.java
@@ -1,21 +1,20 @@
 package simulador.pokemon;
 
-public class TipoPokemon {
-    
-    public static final TipoPokemon FUEGO = new TipoPokemon("Fuego");
-    public static final TipoPokemon AGUA = new TipoPokemon("Agua");
-    public static final TipoPokemon PLANTAVENENO = new TipoPokemon("Planta/Veneno");
-    public static final TipoPokemon ELECTRICO = new TipoPokemon("Eléctrico");
-    public static final TipoPokemon PLANTAPSIQUICO = new TipoPokemon("Planta/Psíquico");
-    public static final TipoPokemon TIERRAROCA = new TipoPokemon("Tierra/Roca");
-    public static final TipoPokemon VOLADORVENENO = new TipoPokemon("Volador/Veneno");
-    public static final TipoPokemon LUCHA = new TipoPokemon("Lucha");
-    public static final TipoPokemon AGUAVENENO = new TipoPokemon("Agua/Veneno");
-    public static final TipoPokemon NORMALVOLADOR = new TipoPokemon("Normal/Volador");
+public enum TipoPokemon {
+    FUEGO("Fuego"),
+    AGUA("Agua"),
+    PLANTAVENENO("Planta/Veneno"),
+    ELECTRICO("Eléctrico"),
+    PLANTAPSIQUICO("Planta/Psíquico"),
+    TIERRAROCA("Tierra/Roca"),
+    VOLADORVENENO("Volador/Veneno"),
+    LUCHA("Lucha"),
+    AGUAVENENO("Agua/Veneno"),
+    NORMALVOLADOR("Normal/Volador");
 
-    private String nombre;
+    private final String nombre;
 
-    private TipoPokemon(String nombre) {
+    TipoPokemon(String nombre) {
         this.nombre = nombre;
     }
 
@@ -24,29 +23,35 @@ public class TipoPokemon {
     }
 
     public static double obtenerMultiplicadorDeDaño(TipoPokemon atacante, TipoPokemon defensor) {
-        if (atacante == FUEGO) {
-            if (defensor == PLANTAVENENO) return 2.0; 
-            if (defensor == AGUA) return 0.5;          
-        } else if (atacante == AGUA) {
-            if (defensor == FUEGO) return 2.0;         
-            if (defensor == PLANTAVENENO) return 0.5;  
-        } else if (atacante == PLANTAVENENO) {
-            if (defensor == AGUA) return 2.0;          
-            if (defensor == FUEGO) return 0.5;         
-        } else if (atacante == ELECTRICO) {
-            if (defensor == AGUAVENENO) return 2.0;    
-            if (defensor == TIERRAROCA) return 0.0;    
-        } else if (atacante == TIERRAROCA) {
-            if (defensor == ELECTRICO) return 2.0;     
-            if (defensor == AGUA) return 0.5;          
-        } else if (atacante == VOLADORVENENO) {
-            if (defensor == LUCHA) return 2.0;         
-            if (defensor == TIERRAROCA) return 0.5;    
-        } else if (atacante == LUCHA) {
-            if (defensor == NORMALVOLADOR) return 2.0; 
+        switch (atacante) {
+            case FUEGO:
+                if (defensor == PLANTAVENENO) return 2.0;
+                if (defensor == AGUA) return 0.5;
+                break;
+            case AGUA:
+                if (defensor == FUEGO) return 2.0;
+                if (defensor == PLANTAVENENO) return 0.5;
+                break;
+            case PLANTAVENENO:
+                if (defensor == AGUA) return 2.0;
+                if (defensor == FUEGO) return 0.5;
+                break;
+            case ELECTRICO:
+                if (defensor == AGUAVENENO) return 2.0;
+                if (defensor == TIERRAROCA) return 0.0;
+                break;
+            case TIERRAROCA:
+                if (defensor == ELECTRICO) return 2.0;
+                if (defensor == AGUA) return 0.5;
+                break;
+            case VOLADORVENENO:
+                if (defensor == LUCHA) return 2.0;
+                if (defensor == TIERRAROCA) return 0.5;
+                break;
+            case LUCHA:
+                if (defensor == NORMALVOLADOR) return 2.0;
+                break;
         }
-
-        return 1.0; 
+        return 1.0;
     }
-    
 }
